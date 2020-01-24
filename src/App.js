08218dfc19695ae599d6a99e3f67742d1d0127ed
= <kd-ict>
2020-01-25 00:11:57 +0200

    add change featur

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          tamot ia zbalaks
+          chanhed on feature yo zbalak
         </p>
         <a
           className="App-link"
